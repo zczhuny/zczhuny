c08e5af9c8b5e247825018a5f4625e94a2295ad0
zczhuny <391302192@qq.com>
2026-09-08 23:52:24 +0800

    第7周作业整改

--- 7a.py
+++ 7a.py
@@ -1,7 +1,7 @@
-with open("mydialiy.txt", "w", encoding="utf-8") as f:
+with open("mydiary.txt", "w", encoding="utf-8") as f:
     f.write("第一行\n")
     f.write("第二行\n")
     f.write("第三行\n")
-for i in open("mydialiy.txt", "r", encoding="utf-8"):
-    i = i.strip()
-    print(i)
+with open("mydiary.txt", "r", encoding="utf-8") as f:
+    for i in f:
+        print(i.strip())
--- 7b.py
+++ 7b.py
@@ -1,6 +1,5 @@
 s = {}
 n = ""
-s_re = ""
 while True:
     n = input("输入姓名（q 退出）：")
     if  n == "q":
@@ -19,13 +18,11 @@ with open("score.txt","r", encoding="utf-8") as f:
 for k2,v2 in s.items():
     print(f"{k2}:{v2}分")
 
-bn = ""
 bs = -1
-if s_re:
+if s:
     for k3,v3 in s.items():
         if v3 > bs:
             bs = v3
-            bn = k3
 
     print(f"最高分：{bs}分")
     print(f"拿到最高分的人：")
